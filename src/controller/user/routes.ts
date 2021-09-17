b5b46e336d84d0a2bc8c4afa5818f9d5ca4e9449
akshay <akshay.patil@successive.tech>
2021-09-17 14:06:03 +0530

    done

--- src/controller/user/routes.ts
+++ src/controller/user/routes.ts
@@ -12,7 +12,7 @@ const router = Router();
 
 router.get('/', authMiddleware(USER, 'read'), validationHandler(validation.get), user.get);
 router.post('/', authMiddleware(USER, 'write'), validationHandler(validation.create), user.post);
-router.put('/:id', authMiddleware(USER, 'write'), validationHandler(validation.dataToUpdate), user.put);
+router.put('/:id', authMiddleware(USER, 'write'), validationHandler(validation.update), user.put);
 router.delete('/:id', authMiddleware(USER, 'delete'), validationHandler(validation.delete), user.delete);
 router.post('/createToken', user.createToken);
 
